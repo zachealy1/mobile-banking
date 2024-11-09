b16152454ec8a6f78ce9555045fac8f70054ca59
Zac Healy <zac.healy@hotmail.co.uk>
2024-11-09 10:35:12 +0000

    Changed statements tab to documents

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/help` | `/(tabs)/settings` | `/(tabs)/statements` | `/_sitemap` | `/help` | `/settings` | `/statements`;
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/documents` | `/(tabs)/help` | `/(tabs)/settings` | `/_sitemap` | `/documents` | `/help` | `/settings`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
